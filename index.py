b4d872543251c8b2be732679a80f825ee1649ff0
Jeremiah Smith <59949075+Jdsmith435@users.noreply.github.com>
2022-06-29 11:24:16 -0600

    refactored

--- index.py
+++ index.py
@@ -1,51 +1,40 @@
-# Start of flight scraper
-import csv
-import time
+# Gets search results based off provided information. 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+from airport_selector import getSelectedAirport
+from greatingMessage import printGreeting
+from console_output import bcolors
 
 # XPaths change when selected
 searchBarInitial = "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[4]/div/div/div[1]/div/div/input"
 searchBarSecondary = "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[2]/div[2]/div[1]/div/input"
-def getAirportsFromCSV():
-    # Get airports from csv file
-    airports = []
-    with open("largeAirports.csv") as csvfile:
-        rows = csv.reader(csvfile)
-        for row in rows:
-            airports.append(row)
+dateStart = "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[2]/div[1]/div[1]/div[1]/div/input"
+dateEnd = "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[2]/div[1]/div[1]/div[2]/div/input"
 
-try:
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.get("https://www.google.com/travel/flights?hl=en-US")
-    search_box = driver.find_element(By.XPATH, searchBarInitial)
-    search_box.click()
-    search_box = driver.find_element(By.XPATH, searchBarSecondary)
-    search_box.click()
-    search_box.send_keys("HONG KONG")
-    #search_box = driver.find_element(By.XPATH,"//div[@aria-label='Enter your destination']//div//input[@aria-label='Where ``else?']")
-    #search_box.send_keys("Hong kong")
+printGreeting()
+getSelectedAirport()
 
-
-#    search_text.send_keys(Keys.RETURN)
-#    print(driver.title)
-    #driver.close()
-except Exception as error:
-    print(bcolors.FAIL, "ERROR: ", error)
+# try:
+#     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+#     driver.get("https://www.google.com/travel/flights?hl=en-US")
+#     search = driver.find_element(By.XPATH, searchBarInitial)
+#     search.click()
+#     search = driver.find_element(By.XPATH, searchBarSecondary)
+#     search.click()
+#     search.send_keys("HONG KONG")
+#     search.send_keys(Keys.TAB)
+#     search = driver.find_element(By.XPATH, dateStart)
+#     # TODO Put date Here 
+#     search.send_keys(Keys.TAB)
+#     search = driver.find_element(By.XPATH, dateEnd)
+#     # TODO Put date Here 
+#     search.send_keys(Keys.TAB)
+#     #driver.close()
+# except Exception as error:
+#     print(bcolors.FAIL, "ERROR: ", error)
 
     
 
