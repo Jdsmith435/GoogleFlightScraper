090c9e40694f0d3c39e78ec8d1a72a9d795f3199
Jeremiah Smith <59949075+Jdsmith435@users.noreply.github.com>
2022-06-29 13:05:41 -0600

    Start of generateDates/index restructure

--- index.py
+++ index.py
@@ -14,34 +14,35 @@ searchBarSecondary = "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2
 dateStart = "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[2]/div[1]/div[1]/div[1]/div/input"
 dateEnd = "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[2]/div[1]/div[1]/div[2]/div/input"
 
+# This function will find flights to a specific airport over a range of dates
+def getPricesForAirport():
+    try:
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.get("https://www.google.com/travel/flights?hl=en-US")
+        search = driver.find_element(By.XPATH, searchBarInitial)
+        search.click()
+        search = driver.find_element(By.XPATH, searchBarSecondary)
+        search.click()
+        search.send_keys(selectedAirport)
+        search.send_keys(Keys.TAB)
+        time.sleep(1)
+        search = driver.find_element(By.XPATH, dateStart)
+        # TODO Put date Here 
+        search.send_keys(Keys.TAB)
+        time.sleep(1)
+        search = driver.find_element(By.XPATH, dateEnd)
+        # TODO Put date Here 
+        search.send_keys(Keys.TAB)
+        time.sleep(1)    
+        destination = driver.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/div[2]/div[2]/ul[1]/li[1]/div/div[2]/div/div[2]/div[3]/span/span[2]/span/span")
+        price = driver.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/div[2]/div[2]/ul[1]/li[1]/div/div[2]/div/div[2]/div[6]/div[1]/div[2]/span")
+        print("SLC => ",destination.text, "   PRICE: ", Fore.GREEN, price.text)
+        print(Style.RESET_ALL)
+        #driver.close()
+    except Exception as error:
+        print(Fore.RED, "ERROR: ", error)
+
+# Start of Call stack
 printGreeting()
 selectedAirport = getSelectedAirport()
-
-try:
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.get("https://www.google.com/travel/flights?hl=en-US")
-    search = driver.find_element(By.XPATH, searchBarInitial)
-    search.click()
-    search = driver.find_element(By.XPATH, searchBarSecondary)
-    search.click()
-    search.send_keys(selectedAirport)
-    search.send_keys(Keys.TAB)
-    time.sleep(1)
-    search = driver.find_element(By.XPATH, dateStart)
-    # TODO Put date Here 
-    search.send_keys(Keys.TAB)
-    time.sleep(1)
-    search = driver.find_element(By.XPATH, dateEnd)
-    # TODO Put date Here 
-    search.send_keys(Keys.TAB)
-    time.sleep(1)    
-    destination = driver.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/div[2]/div[2]/ul[1]/li[1]/div/div[2]/div/div[2]/div[3]/span/span[2]/span/span")
-    price = driver.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/div[2]/div[2]/ul[1]/li[1]/div/div[2]/div/div[2]/div[6]/div[1]/div[2]/span")
-    print("SLC => ",destination.text, "   PRICE: ", Fore.GREEN, price.text)
-    print(Style.RESET_ALL)
-    #driver.close()
-except Exception as error:
-    print(Fore.RED, "ERROR: ", error)
-
-    
-
+getPricesForAirport()
