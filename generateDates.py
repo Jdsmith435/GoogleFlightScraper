e99b635bb435b7e357dbdc3394f75b71141fc9e3
Jeremiah Smith <59949075+Jdsmith435@users.noreply.github.com>
2022-07-09 23:52:38 -0600

    date handler

--- generateDates.py
+++ generateDates.py
@@ -1,2 +1,20 @@
-import calendar
+from datetime import datetime, timedelta, date
+
+class Dates:
+
+    def __init__(self, startDate, endDate):
+            self.startDate = datetime.strptime(startDate, "%m%d%Y").date()
+            self.endDate = datetime.strptime(endDate, "%m%d%Y").date()
+            self.interval = 1
+
+    def __init__(self, startDate, endDate, interval):
+            self.startDate = datetime.strptime(startDate, "%m%d%Y").date()
+            self.endDate = datetime.strptime(endDate, "%m%d%Y").date()
+            self.interval = interval
+
+    def getDays(self):
+        delta = timedelta(days=self.interval)
+        while (self.startDate <= self.endDate):
+            print(self.startDate)
+            self.startDate += delta
 
