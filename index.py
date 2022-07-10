c21814a81541160c8b3f92afcb5ef589480ba24b
Jeremiah Smith <59949075+Jdsmith435@users.noreply.github.com>
2022-07-09 23:53:41 -0600

    Idex to use exterior classes

--- index.py
+++ index.py
@@ -1,48 +1,15 @@
-# Gets search results based off provided information. 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
 from airport_selector import getSelectedAirport
+from driverHandler import WebPageConductor
 from greatingMessage import printGreeting
 from colorama import Fore, Style
-import time
-# XPaths change when selected
-searchBarInitial = "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[4]/div/div/div[1]/div/div/input"
-searchBarSecondary = "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[1]/div[6]/div[2]/div[2]/div[1]/div/input"
-dateStart = "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[2]/div[1]/div[1]/div[1]/div/input"
-dateEnd = "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[2]/div/div[2]/div[1]/div[1]/div[2]/div/input"
-
-# This function will find flights to a specific airport over a range of dates
-def getPricesForAirport():
-    try:
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        driver.get("https://www.google.com/travel/flights?hl=en-US")
-        search = driver.find_element(By.XPATH, searchBarInitial)
-        search.click()
-        search = driver.find_element(By.XPATH, searchBarSecondary)
-        search.click()
-        search.send_keys(selectedAirport)
-        search.send_keys(Keys.TAB)
-        time.sleep(1)
-        search = driver.find_element(By.XPATH, dateStart)
-        # TODO Put date Here 
-        search.send_keys(Keys.TAB)
-        time.sleep(1)
-        search = driver.find_element(By.XPATH, dateEnd)
-        # TODO Put date Here 
-        search.send_keys(Keys.TAB)
-        time.sleep(1)    
-        destination = driver.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/div[2]/div[2]/ul[1]/li[1]/div/div[2]/div/div[2]/div[3]/span/span[2]/span/span")
-        price = driver.find_element(By.XPATH, "/html/body/c-wiz[2]/div/div[2]/c-wiz/div/c-wiz/div[2]/div[2]/ul[1]/li[1]/div/div[2]/div/div[2]/div[6]/div[1]/div[2]/span")
-        print("SLC => ",destination.text, "   PRICE: ", Fore.GREEN, price.text)
-        print(Style.RESET_ALL)
-        #driver.close()
-    except Exception as error:
-        print(Fore.RED, "ERROR: ", error)
-
+from generateDates import Dates
 # Start of Call stack
 printGreeting()
-selectedAirport = getSelectedAirport()
-getPricesForAirport()
+#selectedAirport = getSelectedAirport()
+#currentScraper = WebPageConductor(selectedAirport)
+print(Fore.YELLOW,"Testing with Abeid Airport")
+print(Style.RESET_ALL)
+#currentScraper = WebPageConductor('Abeid Amani Karume International Airport')
+#currentScraper.getPricesForAirport()
+datesGenerated = Dates("07122022", "09122022",7)
+datesGenerated.getDays()
