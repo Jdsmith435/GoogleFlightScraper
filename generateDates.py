591b59ba17f7cfe5aa223792b65d9681833bef1d
Jeremiah Smith <59949075+Jdsmith435@users.noreply.github.com>
2022-07-13 13:08:52 -0600

    Added getters, removed OL constructor

--- generateDates.py
+++ generateDates.py
@@ -1,20 +1,24 @@
 from datetime import datetime, timedelta, date
 
 class Dates:
+        # Constructor creating self fields to start date, end date
+        def __init__(self, startDate, endDate):
+                self.startDate = datetime.strptime(startDate, "%y%m%d").date()
+                self.endDate = datetime.strptime(endDate, "%y%m%d").date()
+                self.interval = 1
+                
+        # Yields a currunt end trip date to get a price back
+        def getDays(self):
+                delta = timedelta(days=self.interval)
+                while (self.startDate <= self.endDate):
+                        yield self.startDate
+                        self.startDate += delta
 
-    def __init__(self, startDate, endDate):
-            self.startDate = datetime.strptime(startDate, "%m%d%Y").date()
-            self.endDate = datetime.strptime(endDate, "%m%d%Y").date()
-            self.interval = 1
-
-    def __init__(self, startDate, endDate, interval):
-            self.startDate = datetime.strptime(startDate, "%m%d%Y").date()
-            self.endDate = datetime.strptime(endDate, "%m%d%Y").date()
-            self.interval = interval
-
-    def getDays(self):
-        delta = timedelta(days=self.interval)
-        while (self.startDate <= self.endDate):
-            print(self.startDate)
-            self.startDate += delta
-
+        def startDayGetter(self):
+                print(self.startDate.__str__())
+                return self.startDate.__str__()
+        
+        def endDayGetter(self):
+                print(self.endDate.__str__())
+                return self.endDate.__str__()
+        
